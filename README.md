dc5e697bef4eb29b9427013982e738961cf5f477
deepinvalue <deepinvalue@protonmail.com>
2023-11-19 04:39:02 +0330

    Initial Commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Doc2Speech
+
+Doc2Speech is a Python-based tool that converts HTML documents, and soon EPUB files, into natural-sounding speech. It leverages OpenAI's advanced Text-to-Speech (TTS) model and is specifically designed to handle the character limit constraints of the API, ensuring a seamless and natural flow of speech.
+
+## Features
+
+- **Convert HTML to Speech**: Turns any HTML content into natural-sounding audio.
+- **Direct URL Processing**: Capable of directly fetching and converting HTML content from URLs.
+- **OpenAI's TTS Model**: Utilizes the latest in TTS technology for high-quality speech synthesis.
+- **Character Limit Management**: Smartly segments texts at sentence boundaries to respect OpenAI's character limits without disrupting the narrative flow.
+- **Customizable Content Processing**: Ability to exclude specific HTML tags, classes, or IDs.
+- **Estimated Cost Calculation**: Provides an estimated cost calculation for using OpenAI's TTS API.
+- **EPUB Support Coming Soon**: Future updates will include EPUB document conversion.
+
+## Getting Started
+
+### Prerequisites
+
+Ensure you have Python 3.x installed on your system. The following Python libraries are required:
+
+- BeautifulSoup4
+- OpenAI Python client
+- pydub
+- tqdm
+- requests
+
+### Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/deepinvalue/Doc2Speech.git
+   ```
+2. Navigate to the cloned directory and install the required packages:
+   ```bash
+   pip install -r requirements.txt
+   ```
+## Configuration
+
+Rename `config.json.sample` to `config.json` and update it as follows:
+
+- `api_key`: Your OpenAI API key.
+- `tts_model`: The TTS model to use ("tts-1", "tts-1-hd").
+- `voice`: The voice model for TTS ("alloy", "echo", "fable", "onyx", "nova", "shimmer").
+- `audio_format`: Desired audio format for the output ("mp3", "opus", "aac", "flac").
+- `excluded_tags`: List of HTML tags to exclude from text conversion (e.g., ["pre", "header", "nav"]).
+- `excluded_classes`: List of HTML classes to exclude (e.g., ["class1", "class2"]).
+- `excluded_ids`: List of HTML element IDs to exclude (e.g., ["id1", "id2"]).
+- `price_per_1k_chars`: The price per 1000 characters for using the OpenAI TTS API.
+- `max_character_limit`: The maximum number of characters to process in a single TTS request.
+- `user_agent`: Custom user agent string for directly fetching web content, if empty defaults to system's user agent.
+
+## Usage
+
+   ```bash
+   python doc2speech.py <input-file-or-url> -o <output-directory>
+   ```
+   Options:
+
+   - `<input-file-or-url>`: Path to the HTML file or URL.
+   - `<output-directory>`: Directory where the output audio file will be saved.
+
+## Future Enhancements
+
+- Addition of EPUB file support.
+- Expansion of audio format support beyond MP3.
+
+## Contributing
+
+Contributions are welcome. Please feel free to fork the repository and submit pull requests.
---
+++ doc2speech.py
@@ -0,0 +1,125 @@
+import os
+import re
+import json
+import argparse
+from tqdm import tqdm
+from pathlib import Path
+from bs4 import BeautifulSoup
+from pydub import AudioSegment
+from openai._client import OpenAI
+
+
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        return json.load(file)
+
+def user_confirmation():
+    while True:
+        user_input = input("Do you want to continue with the TTS conversion? [Y/n]: ").lower().strip()
+        if user_input in ['', 'yes', 'y']:
+            return True
+        elif user_input in ['no', 'n']:
+            return False
+        else:
+            print("Please enter 'yes' (or press enter for default), or 'no'.")   
+
+def is_url(path):
+    return path.startswith('http://') or path.startswith('https://')
+
+def get_content(input_path, user_agent):
+    if is_url(input_path):
+        import requests
+        headers = {}
+        if user_agent:
+            headers['User-Agent'] = user_agent        
+        response = requests.get(input_path, headers=headers)
+        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+        return response.text
+    else:
+        return Path(input_path).read_text()
+
+def clean_html_text(html_content, excluded_tags, excluded_classes, excluded_ids):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    # Remove specified tags
+    for tag in excluded_tags:
+        for element in soup.find_all(tag):
+            element.decompose()
+    # Remove elements with specified classes
+    for class_name in excluded_classes:
+        for element in soup.find_all(class_=class_name):
+            element.decompose()
+    # Remove elements with specified IDs
+    for element_id in excluded_ids:
+        element = soup.find(id=element_id)
+        if element:
+            element.decompose()            
+    return soup.get_text()
+
+def calculate_cost(text_length, price_per_1k_chars):
+    return price_per_1k_chars * text_length / 1000
+
+def process_text_to_speech(client, text, tts_model, voice, chunk_size, output_dir):
+    def split_text(text):
+        chunks = []
+        while text:
+            if len(text) <= chunk_size:
+                chunks.append(text)
+                break
+            split_index = max(text.rfind('.', 0, chunk_size) + 1, text.rfind(' ', 0, chunk_size))
+            chunk, text = text[:split_index], text[split_index:]
+            chunks.append(chunk)
+        return chunks
+
+    def process_chunk(chunk, index):
+        speech_file_path = output_dir / f'speech_chunk_{index}.mp3'
+        response = client.audio.speech.create(model=tts_model, voice=voice, input=chunk)
+        response.stream_to_file(speech_file_path)
+        return speech_file_path
+
+    audio_chunks = []
+    for index, chunk in enumerate(tqdm(split_text(text), desc="Processing Text Chunks", unit="chunk")):
+        audio_chunks.append(process_chunk(chunk, index)) 
+    combined_audio = AudioSegment.empty()
+    for file_path in audio_chunks:
+        combined_audio += AudioSegment.from_mp3(file_path)
+        os.remove(file_path)
+    return combined_audio
+
+def main():
+    parser = argparse.ArgumentParser(description='Process HTML file to speech.')
+    parser.add_argument('input', help='Path to the input file (HTML or text)')
+    parser.add_argument('-o', '--output-dir', default=Path(__file__).parent / 'output', help='Path to the output directory')
+    parser.add_argument('--clean-html', type=bool, default=True, help='Whether to clean HTML content and remove extra whitespace (default: True)')
+    parser.add_argument('--config', default='config.json', help='Path to the configuration file')
+    args = parser.parse_args()
+
+    config = load_config(args.config)
+    client = OpenAI(api_key=config['api_key'])
+
+    # Ensure output directory exists
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Fetch content from URL or local file
+    input_content = get_content(args.input, config.get('user_agent', ''))
+
+    if args.clean_html:
+        clean_text = clean_html_text(input_content, config['excluded_tags'], config['excluded_classes'], config['excluded_ids']).strip()
+        clean_text = re.sub(r'\n\s*\n', '\n', clean_text)
+        clean_text = re.sub(r'\s\s+', ' ', clean_text)
+    else:
+        clean_text = input_content.strip()        
+
+    total_cost = calculate_cost(len(clean_text), config['price_per_1k_chars'])
+    print(f"Estimated Cost: {total_cost:.3f}$")
+
+    if user_confirmation():
+        audio = process_text_to_speech(client, clean_text, config['tts_model'], config['voice'], config['max_character_limit'], output_dir)
+        output_file = output_dir / f"{Path(args.input).stem}.{config['audio_format']}"
+        audio.export(output_file, format=config['audio_format'])
+        print(f"TTS conversion finished successfully. File saved to {output_file}")
+    else:
+        print("TTS conversion canceled.")
+
+if __name__ == '__main__':
+    main()
